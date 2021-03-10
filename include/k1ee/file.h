16223a489a97435edbf6f2236c04a9053198f209
Schwarzer <wangzwnj@126.com>
2021-03-10 16:26:22 +0800

    read_all_bytes templatized

--- include/k1ee/file.h
+++ include/k1ee/file.h
@@ -3,9 +3,34 @@
 #include <vector>
 #include <cstdint>
 #include <filesystem>
+#include <fstream>  
+
+#include "k1ee/exception.h"  
 
 namespace k1ee
-{
-std::vector<uint8_t> read_all_bytes(const std::filesystem::path& path);
-void write_all_bytes(const std::filesystem::path& path, const uint8_t* data, size_t size);
+{ 
+template <typename DataType = uint8_t> requires (sizeof(DataType) == 1)
+std::vector<DataType> read_all_bytes(const std::filesystem::path& path)
+{  
+	if(!exists(path) || is_directory(path))
+		throw k1ee_runtime_error("file doesn't exist."); 
+
+	auto size = file_size(path);
+	
+	std::vector<DataType> ret(size);
+
+	std::ifstream fin(path, std::ios::binary);
+
+	if(fin.fail())
+		throw k1ee_runtime_error("open file failed");
+
+	fin.read(reinterpret_cast<char*>(ret.data()), size);
+
+	if(fin.fail())
+		throw k1ee_runtime_error("read file failed");
+	
+	return ret;
+}
+	
+void write_all_bytes(const std::filesystem::path& path, const void* data, size_t size);
 }
--- src/file.cpp
+++ src/file.cpp
@@ -1,35 +1,11 @@
 ﻿#include "k1ee/k1ee.h"  
-#include <fstream> 
+#include <fstream>  
 
-std::vector<uint8_t> k1ee::read_all_bytes(const std::filesystem::path& path)
-{
-	using namespace std::filesystem;
-	
-	if(!exists(path) || is_directory(path))
-		throw k1ee::k1ee_runtime_error("file doesn't exist."); 
-
-	auto size = file_size(path);
-	
-	std::vector<uint8_t> ret(size);
-
-	std::ifstream fin(path, std::ios::binary);
-
-	if(fin.fail())
-		throw k1ee::k1ee_runtime_error("open file failed");
-
-	fin.read(reinterpret_cast<char*>(ret.data()), size);
-
-	if(fin.fail())
-		throw k1ee::k1ee_runtime_error("read file failed");
-	
-	return ret;
-}
-
-void k1ee::write_all_bytes(const std::filesystem::path& path, const uint8_t* data, size_t size)
+void k1ee::write_all_bytes(const std::filesystem::path& path, const void* data, size_t size)
 {
 	using namespace std::filesystem;
 
 	std::ofstream fout(path, std::ios::binary);
 
-	fout.write(reinterpret_cast<const char*>(data), size);
+	fout.write(static_cast<const char*>(data), size);
 }
